e479cb519836dd11684f12b6c19f8758f5e4f075
XiscaObrador <60427705+XiscaObrador@users.noreply.github.com>
2021-12-13 13:16:47 +0100

    afegir js i bootrsap

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+const { series } = require('gulp');
 var gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
 
@@ -11,7 +12,12 @@ function buildStyles2() {
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 };
-
+function copy() { // copiam arxis pq mos funcioni es js a netlift //
+  return gulp.src('./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js')
+    .pipe(gulp.dest('./js'));
+};
 
 exports.compila = buildStyles;
 exports.compila2 = buildStyles2;
+exports.copy = copy;
+exports.build = series(buildStyles2, copy);
